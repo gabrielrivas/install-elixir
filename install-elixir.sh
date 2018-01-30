fa034f76b6ffcaa4117eeaf38c2367baba2cc6fd
gabrielrivas <gabriel.rivas@ascom.com>
2018-01-30 09:42:47 -0500

    Add more lines to the script

--- install-elixir.sh
+++ install-elixir.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
 apt-get update
-sudo apt-get install elixir
+apt-get install esl-erlang
+apt-get install elixir
