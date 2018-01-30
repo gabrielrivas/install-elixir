2d852aff23cee7579bdc7917de7f034ddb2a9187
gabrielrivas <gabriel.rivas@ascom.com>
2018-01-30 09:46:10 -0500

    Remove all sudo

--- install-elixir.sh
+++ install-elixir.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && sudo dpkg -i erlang-solutions_1.0_all.deb
+wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && dpkg -i erlang-solutions_1.0_all.deb
 apt-get update
 apt-get install esl-erlang
 apt-get install elixir
